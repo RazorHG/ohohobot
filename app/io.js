7fd12e18529f30d7a6f27752820a2e58f4d30459
RazorHG <boundtogo@gmail.com>
2017-06-21 21:51:20 -0400

    plays a random file in sounds folder

--- app/io.js
+++ app/io.js
@@ -16,30 +16,34 @@ bot.on('message', function(user, userID, channelID, message, event) {
             to: channelID,
             message: "OHOHOHOHO! :laughing:"
         });
-		var user = _.find(bot.users, {id: userID});
-		var voiceChannelUserIsIn = _.find(bot.channels, function(channel){
-			return channel.type === "voice" && _.find(channel.members, {user_id: user.id});
-		});
-		if(voiceChannelUserIsIn){
-			console.log(user.username + "in" + voiceChannelUserIsIn.id);
-			bot.joinVoiceChannel(voiceChannelUserIsIn.id, function(){
-				console.log("joined " + voiceChannelUserIsIn.name);
-				//Then get the audio context
-				bot.getAudioContext(voiceChannelUserIsIn.id, function(error, stream) {
-					//Once again, check to see if any errors exist
-					if (error) return console.error(error);
+        var user = _.find(bot.users, { id: userID });
+        var voiceChannelUserIsIn = _.find(bot.channels, function(channel) {
+            return channel.type === "voice" && _.find(channel.members, { user_id: user.id });
+        });
+        if (voiceChannelUserIsIn) {
+            console.log(user.username + "in" + voiceChannelUserIsIn.id);
+            bot.joinVoiceChannel(voiceChannelUserIsIn.id, function() {
+                console.log("joined " + voiceChannelUserIsIn.name);
+                //Then get the audio context
+                bot.getAudioContext(voiceChannelUserIsIn.id, function(error, stream) {
+                    //Once again, check to see if any errors exist
+                    if (error) return console.error(error);
 
-					//Create a stream to your file and pipe it to the stream
-					//Without {end: false}, it would close up the stream, so make sure to include that.
-					fs.createReadStream('sounds/ohoho.mp3').pipe(stream, {end: false});
+                    fs.readdir("./sounds", function(err, items) {
+                        var randomSoundFile = items[_.random(0, items.length - 1)];
+                        //Create a stream to your file and pipe it to the stream
+                        //Without {end: false}, it would close up the stream, so make sure to include that.
+                        fs.createReadStream('sounds/' + randomSoundFile).pipe(stream, { end: false });
 
-					//The stream fires `done` when it's got nothing else to send to Discord.
-					stream.on('done', function() {
-					bot.leaveVoiceChannel(voiceChannelUserIsIn.id);
-					});
-				});
-			});
-		}
-		
+                        //The stream fires `done` when it's got nothing else to send to Discord.
+                        stream.on('done', function() {
+                            bot.leaveVoiceChannel(voiceChannelUserIsIn.id);
+                        });
+                    });
+
+                });
+            });
+        }
+
     }
 });
